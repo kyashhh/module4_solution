3f3123ad92e512fa3d1d26e594dd44ad95add31c
kyashhh <92942252+kyashhh@users.noreply.github.com>
2022-01-30 19:17:45 +0530

    Add files via upload

---
+++ js/SpeakGoodBye.js
@@ -0,0 +1,11 @@
+(function(){
+var byeSpeaker = {};
+byeSpeaker.speakWord = "Good Bye";
+
+byeSpeaker.speak = function (name) {
+  console.log(byeSpeaker.speakWord + " " + name);
+}
+
+window.byeSpeaker = byeSpeaker;
+
+})();
---
+++ js/SpeakHello.js
@@ -0,0 +1,11 @@
+(function (){
+var helloSpeaker = {};
+
+helloSpeaker.speakWord = "Hello";
+
+helloSpeaker.speak = function (name) {
+  console.log(helloSpeaker.speakWord + " " + name);
+}
+
+window.helloSpeaker = helloSpeaker;
+})();
